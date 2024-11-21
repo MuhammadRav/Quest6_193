e59f89399572fdacdd65580733c6edc4b0c05bc3
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-11-21 19:29:39 +0700

    membuat jarak tampilan

--- app/src/main/java/com/example/week8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/week8/ui/view/screen/MahasiswaFormView.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -54,17 +55,18 @@ fun MahasiswaFormView(
             color = colorResource(
                 R.color.greenTerang
             )
-        )
+        ), horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Row (modifier = Modifier
-            .padding(20.dp),
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row (
             verticalAlignment = Alignment.CenterVertically
         ){
             Image(
                 painter = painterResource(
                     R.drawable.logo
                 ),
-                contentDescription = ""
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
             )
             Spacer(modifier = Modifier.padding(8.dp))
             Column {

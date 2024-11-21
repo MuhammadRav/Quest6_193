1bf1264f68663b7cfd7a7f96dd3ca5b9a20e9c1c
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-11-21 19:44:08 +0700

    membuat parameter

--- app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
@@ -1,2 +1,13 @@
 package com.example.week8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.example.week8.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}

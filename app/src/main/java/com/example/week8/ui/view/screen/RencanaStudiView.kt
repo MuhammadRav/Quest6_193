9784c07f684e86695597fbdb26437037811658be
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-11-21 19:51:19 +0700

    menambahkan foto

--- app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
@@ -1,6 +1,21 @@
 package com.example.week8.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.week8.R
 import com.example.week8.model.Mahasiswa
 
 @Composable
@@ -9,5 +24,27 @@ fun RencanaStudyView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
-
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+    var checked by remember { mutableStateOf(false)}
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(16.dp), verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(R.drawable.aku),
+            contentDescription = "",
+            modifier = Modifier
+                .clip(shape = CircleShape)
+                .size(50.dp)
+        )
+    }
 }

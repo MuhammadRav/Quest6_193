17ffc2851d30b95897b15e99f6fffcde19a0192d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-11-21 19:54:27 +0700

    menambahkan teks nama dan nim

--- app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/week8/ui/view/screen/RencanaStudiView.kt
@@ -3,9 +3,12 @@ package com.example.week8.ui.view.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,8 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.week8.R
 import com.example.week8.model.Mahasiswa
 
@@ -46,5 +52,20 @@ fun RencanaStudyView(
                 .clip(shape = CircleShape)
                 .size(50.dp)
         )
+        Spacer(modifier = Modifier.padding(start = 16.dp))
+        Column (modifier = Modifier.weight(1f)){
+            Text(
+                text = mahasiswa.nama,
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                color = Color.White
+            )
+            Text(
+                text = mahasiswa.nim,
+                fontWeight = FontWeight.Light,
+                fontSize = 12.sp,
+                color = Color.White
+            )
+        }
     }
 }
